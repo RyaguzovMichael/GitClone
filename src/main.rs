cf115bcf55726759982ae63b70b71c84cf98713f
Ryaguzov  Michael <miha890r@gmail.com>
2024-01-03 18:06:13 +0600

    Refactor naming

--- src/main.rs
+++ src/main.rs
@@ -4,38 +4,38 @@ use std::process::Command;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let git_path = match args.get(1) {
+    let git_url = match args.get(1) {
         Some(x) => x,
-        None => panic!("Input git path is empty"),
+        None => panic!("Input git url is empty"),
     };
-    let work_directory = get_work_directory(&args[0]);
-    let project_name = get_project_name(git_path).to_lowercase();
-    let target = get_target(&work_directory);
-    let destination_path = format!("{target}\\{project_name}");
+    let project_name = get_project_name(git_url).to_lowercase();
+    let default_destination_path = get_default_destination_path(&args[0]);
+    let destination_path = format!("{default_destination_path}\\{project_name}");
 
-    run_git_clone_command(&git_path, &destination_path);
+    run_git_clone_command(&git_url, &destination_path);
 }
 
-fn get_work_directory(executable_path: &str) -> &str {
-    let last_index = executable_path.rfind('\\').unwrap();
-    &executable_path[..last_index]
+fn get_project_name(git_url: &str) -> &str {
+    let start_index_of_project_name = git_url.rfind('/').unwrap() + 1;
+    let end_index_of_project_name = git_url.rfind('.').unwrap();
+    &git_url[start_index_of_project_name..end_index_of_project_name]
 }
 
-fn get_project_name(git_path: &str) -> &str {
-    let start_index_of_project_name = git_path.rfind('/').unwrap() + 1;
-    let end_index_of_project_name = git_path.rfind('.').unwrap();
-    &git_path[start_index_of_project_name..end_index_of_project_name]
-}
-
-fn get_target(work_directory: &str) -> String {
-    let config_path = format!("{}\\target.config", work_directory);
+fn get_default_destination_path(executable_file_path: &str) -> String {
+    let executable_directory = get_executable_directory(executable_file_path);
+    let config_path = format!("{executable_directory}\\target.config");
     fs::read_to_string(config_path).unwrap()
 }
 
-fn run_git_clone_command(git_path: &str, destination_path: &str) {
+fn get_executable_directory(executable_file_path: &str) -> &str {
+    let last_index = executable_file_path.rfind('\\').unwrap();
+    &executable_file_path[..last_index]
+}
+
+fn run_git_clone_command(git_url: &str, destination_path: &str) {
     let output = Command::new("git")
         .arg("clone")
-        .arg(git_path)
+        .arg(git_url)
         .arg(destination_path)
         .output()
         .expect("Failed to execute command");
