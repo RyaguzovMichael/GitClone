7fe940c1484684f5f24eabf7a1484ac570fa79bd
Ryaguzov  Michael <miha890r@gmail.com>
2024-01-03 17:37:06 +0600

    Refactor code to use string slices

--- src/main.rs
+++ src/main.rs
@@ -4,41 +4,42 @@ use std::process::Command;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let git_path = args.get(1).unwrap();
-    let work_directory = get_work_directory(&args);
-    let project_name = get_project_name(git_path);
+    let git_path = match args.get(1) {
+        Some(x) => x,
+        None => panic!("Input git path is empty"),
+    };
+    let work_directory = get_work_directory(&args[0]);
+    let project_name = get_project_name(git_path).to_lowercase();
     let target = get_target(&work_directory);
-    let destination_path = format!("{}\\{}", target, project_name);
+    let destination_path = format!("{target}\\{project_name}");
 
+    run_git_clone_command(&git_path, &destination_path);
+}
+
+fn get_work_directory(executable_path: &str) -> &str {
+    let last_index = executable_path.rfind('\\').unwrap();
+    &executable_path[..last_index]
+}
+
+fn get_project_name(git_path: &str) -> &str {
+    let start_index_of_project_name = git_path.rfind('/').unwrap() + 1;
+    let end_index_of_project_name = git_path.rfind('.').unwrap();
+    &git_path[start_index_of_project_name..end_index_of_project_name]
+}
+
+fn get_target(work_directory: &str) -> String {
+    let config_path = format!("{}\\target.config", work_directory);
+    fs::read_to_string(config_path).unwrap()
+}
+
+fn run_git_clone_command(git_path: &str, destination_path: &str) {
     let output = Command::new("git")
         .arg("clone")
         .arg(git_path)
-        .arg(&destination_path)
+        .arg(destination_path)
         .output()
         .expect("Failed to execute command");
 
-    println!("status: {}", output.status);
     stdout().write_all(&output.stdout).unwrap();
     stderr().write_all(&output.stderr).unwrap();
-}
-
-fn get_work_directory(args: &Vec<String>) -> String {
-    let executable_path = args.get(0).unwrap();
-    let last_index = executable_path.rfind('\\').unwrap();
-    executable_path[..last_index].to_string()
-}
-
-fn get_project_name(git_path: &String) -> String {
-    if !git_path.ends_with(".git") {
-        panic!("Unknown path to git")
-    }
-
-    let start_index_of_project_name = git_path.rfind('/').unwrap() + 1;
-    let end_index_of_project_name = git_path.rfind('.').unwrap();
-    git_path[start_index_of_project_name..end_index_of_project_name].to_lowercase().clone()
-}
-
-fn get_target(work_directory: &String) -> String {
-    let config_path = format!("{}\\target.config", work_directory);
-    fs::read_to_string(config_path).unwrap()
 }
