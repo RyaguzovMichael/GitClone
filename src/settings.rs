2a937792939530583df68a00cc6da2f7b7117795
Ryaguzov  Michael <miha890r@gmail.com>
2025-04-11 21:55:02 +0500

    Small refactor config loading

--- src/settings.rs
+++ src/settings.rs
@@ -1,9 +1,9 @@
+use serde::{Deserialize, Serialize};
 use std::{
-    fs::{self, File},
-    io::{self, ErrorKind, Read, Write},
+    fs::{self, File, OpenOptions},
+    io::{self, Read, Write},
     path::{Path, PathBuf},
 };
-use serde::{Deserialize, Serialize};
 
 use crate::Error;
 
@@ -35,12 +35,12 @@ impl Config {
 
     fn deserialize(file_data: &str) -> Result<Config, Error> {
         toml::from_str(file_data)
-            .map_err(|_| Error::from("Can't be deserialize config"))
+            .map_err(|e| Error::from(format!("Can't be deserialize config: {}", e).as_str()))
     }
 
     fn serialize(&self) -> Result<String, Error> {
         toml::to_string_pretty(self)
-            .map_err(|_| Error::from("Can't be serialize config"))
+            .map_err(|e| Error::from(format!("Can't be serialize config: {}", e).as_str()))
     }
 
     fn from_home_dir(home_dir: &Path) -> Self {
@@ -60,16 +60,16 @@ fn get_config_file_data(config_path: &Path) -> Result<String, io::Error> {
 
 fn save_config_file(config_path: &Path, config: &str) -> Result<(), io::Error> {
     let file_path = config_path.join(CONFIG_FILE_NAME);
-    let mut file = match File::open(&file_path) {
-        Ok(file) => file,
-        Err(e) if e.kind() == ErrorKind::NotFound => {
-            if !fs::exists(&config_path)? {
-                fs::create_dir(&config_path)?;
-            }
-            File::create(&file_path)?
-        }
-        Err(e) => return Err(e),
-    };
-    file.write(config.as_bytes())?;
+
+    if !config_path.exists() {
+        fs::create_dir_all(config_path)?
+    }
+    let mut file = OpenOptions::new()
+        .write(true)
+        .create(true)
+        .truncate(true)
+        .open(&file_path)?;
+
+    file.write_all(config.as_bytes())?;
     Ok(())
 }
